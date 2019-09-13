323d7a5b2c9c5820ccd4cbc73b0ed321be99ee7e
Garrett Thompson <gtcruz.gt@gmail.com>
2019-09-13 23:48:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
 
   attr_accessor :sender, :receiver, :transfer
 
-  def initialize(transfer, sender, receiver)
+  def initialize(transfer, sender="", receiver="")
     @transfer = transfer
     @sender = sender
     @receiver = receiver
