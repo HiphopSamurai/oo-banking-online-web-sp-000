b01ba8f4f82b99643dd7cabd7616afc386ea476f
Garrett Thompson <gtcruz.gt@gmail.com>
2019-09-14 00:53:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,14 +22,19 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
+    #if @sender.balance > @amount && @status == "pending"
+    #  @sender.balance -= @amount
+    #  @receiver.balance += @amount
+    #  @status = "complete"
+    #else
+    #  @status = "rejected"
+    #  return "Transaction rejected. Please check your account balance."
+    #end
+
+    if @sender.balance < @amount
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
   end
 
 
